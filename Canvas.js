7ed9d850a0e1f2a549bc44f49632158bd1938ca8
Luke Taylor <luke@circleleasing.co.uk>
2023-08-31 13:35:40 +0100

    Add Canvas class to take responsibility for actual drawing

---
+++ Canvas.js
@@ -0,0 +1,81 @@
+class Canvas {
+	#el = null;
+	#ctx = null;
+	#scale = 0;
+	#targetW = 0;
+	#targetH = 0;
+
+	constructor(id, targetW, targetH) {
+		this.#el = document.getElementById(id);
+		this.#ctx = this.#el.getContext("2d");
+		this.#el.addEventListener("click", this.#el.requestFullscreen);
+
+		this.#targetW = targetW;
+		this.#targetH = targetH;
+	}
+
+	setScale(scale) {
+		const w = this.#targetW * scale;
+		const h = this.#targetH * scale;
+		const ratio = Math.ceil(window.devicePixelRatio);
+
+		this.#el.width = w * ratio;
+		this.#el.height = h * ratio;
+
+		this.#ctx.scale(ratio, ratio);
+
+		this.#el.style.width = `${w}px`;
+		this.#el.style.height = `${h}px`;
+
+		this.#scale = scale * ratio;
+	}
+
+	setFont(font) {
+		this.#ctx.font = font;
+	}
+
+	setTextAlign(alignment) {
+		this.#ctx.textAlign = alignment;
+	}
+
+	setFill(fill) {
+		this.#ctx.fillStyle = fill;
+	}
+
+	drawRect(x, y, w, h, colour = null) {
+		if (colour != null) {
+			this.setFill(colour);
+		}
+
+		this.#ctx.fillRect(x * this.#scale, y * this.#scale, w * this.#scale, h * this.#scale);
+	}
+
+	drawCircle(x, y, radius, colour = null) {
+		if (colour != null) {
+			this.setFill(colour);
+		}
+
+		this.#ctx.arc((x + (radius / 2)) * this.#scale, y * this.#scale, radius * this.#scale, 0, Math.PI * 2);
+		this.#ctx.fill();
+	}
+
+	drawText(text, x, y, { 
+		colour = null, 
+		alignment = null, 
+		font = null 
+	} = {}) {
+		if (colour != null) {
+			this.setFill(colour);
+		}
+
+		if (alignment != null) {
+			this.setTextAlign(alignment);
+		}
+
+		if(font != null){
+			this.setFont(font);
+		}
+
+		this.#ctx.fillText(text, x * this.#scale, y * this.#scale);
+	}
+}
